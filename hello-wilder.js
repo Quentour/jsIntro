e3eb9f61589dd362f544e29fa9524eb1ea6d4538
Quentour <48018165+Quentour@users.noreply.github.com>
2019-03-15 10:49:00 +0100

    Create hello-wilder.js

---
+++ hello-wilder.js
@@ -0,0 +1,10 @@
+/* hello-wilder.js */
+
+// Define some variables
+const movie = 'Fist of Fury';
+const releaseYear = 1972
+const director = 'Wo Lei';
+const message = movie + ' directed by ' + director + ' release in ' + releaseYear;
+
+// Display a popup
+alert(message);
